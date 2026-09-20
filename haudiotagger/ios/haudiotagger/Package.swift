34b833e121641cbb7264fdd98d89b98c7313ad50
Hirdaya Shrestha <hirdaya098@gmail.com>
2026-09-20 20:22:42 +0545

    fix: windows cmake, macos and ios package fix

--- haudiotagger/ios/haudiotagger/Package.swift
+++ haudiotagger/ios/haudiotagger/Package.swift
@@ -1,10 +1,13 @@
-// swift-tools-version:5.9
+// swift-tools-version: 5.9
 import PackageDescription
 
 let package = Package(
     name: "haudiotagger",
     platforms: [.iOS(.v12)],
-    libraries: [
+    products: [
+        .library(name: "haudiotagger", targets: ["haudiotagger"])
+    ],
+    targets: [
         .binaryTarget(
             name: "haudiotagger",
             path: "Frameworks/haudiotagger.xcframework"
--- haudiotagger/macos/haudiotagger/Package.swift
+++ haudiotagger/macos/haudiotagger/Package.swift
@@ -1,10 +1,13 @@
-// swift-tools-version:5.9
+// swift-tools-version: 5.9
 import PackageDescription
 
 let package = Package(
     name: "haudiotagger",
     platforms: [.macOS(.v10_14)],
-    libraries: [
+    products: [
+        .library(name: "haudiotagger", targets: ["haudiotagger"])
+    ],
+    targets: [
         .binaryTarget(
             name: "haudiotagger",
             path: "Frameworks/haudiotagger.xcframework"
